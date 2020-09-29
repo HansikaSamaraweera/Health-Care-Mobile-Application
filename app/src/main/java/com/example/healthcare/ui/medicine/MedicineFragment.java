2d474bec6e25b58e6db014b1de95df99a040d998
Hansika_Samaraweera <hansika.m99@gmail.com>
2020-09-29 21:01:13 +0530

    Medicine

--- app/src/main/java/com/example/healthcare/ui/medicine/MedicineFragment.java
+++ app/src/main/java/com/example/healthcare/ui/medicine/MedicineFragment.java
@@ -1,16 +1,25 @@
 package com.example.healthcare.ui.medicine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.example.healthcare.CalenderActivity;
+import com.example.healthcare.MedicineActivity;
+import com.example.healthcare.MedicineHome;
 import com.example.healthcare.R;
+import com.example.healthcare.ui.home.HomeFragment;
 
 public class MedicineFragment extends Fragment {
 
@@ -31,10 +40,20 @@ public class MedicineFragment extends Fragment {
                 ViewModelProviders.of(this).get(MedicineViewModel.class);
         View root = inflater.inflate(R.layout.fragment_medicine, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
+        final Button save=root.findViewById(R.id.savem);
         medicineViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+
+                save.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent i = new Intent(getActivity(), MedicineActivity.class);
+                        startActivity(i);
+               
+                   }
+                });
             }
         });
         return root;
